973b5bcd0d4c7ea6101e94d247a24e63a253afa0
Buhari Hafiz Rano <buhafran@gmail.com>
2026-07-12 12:01:19 +0100

    updated livewire

--- backend/config/livewire.php
+++ backend/config/livewire.php
@@ -25,19 +25,44 @@ return [
     |
     */
 
-    'temporary_file_upload' => [
-        'disk' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_DISK', 'local'),
-        'rules' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_RULES', 'file|max:30720'),
-        'directory' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_DIRECTORY', 'livewire-tmp'),
-        'middleware' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_MIDDLEWARE', 'throttle:60,1'),
-        'preview_mimes' => [
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-        ],
-        'max_upload_time' => (int) env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_MAX_TIME', 5),
-        'cleanup' => true,
+  'temporary_file_upload' => [
+    'disk' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_DISK', 'local'),
+
+    'rules' => env(
+        'LIVEWIRE_TEMPORARY_FILE_UPLOAD_RULES',
+        'file|max:30720'
+    ),
+
+    'directory' => env(
+        'LIVEWIRE_TEMPORARY_FILE_UPLOAD_DIRECTORY',
+        'livewire-tmp'
+    ),
+
+    'middleware' => [
+        'throttle:60,1',
     ],
+
+    'preview_mimes' => [
+        'png',
+        'gif',
+        'bmp',
+        'svg',
+        'wav',
+        'mp4',
+        'mov',
+        'avi',
+        'wmv',
+        'mp3',
+        'm4a',
+        'jpg',
+        'jpeg',
+        'mpga',
+        'webp',
+        'wma',
+    ],
+
+    'max_upload_time' => 5,
+],
 
     'render_on_redirect' => false,
 
